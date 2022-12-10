2ab85262b43a928597a220824fd38b9323b5a667
JiddanFaqihHaikal <116089926+JiddanFaqihHaikal@users.noreply.github.com>
2022-12-10 13:41:59 +0700

    Update TransportasiAir.java

--- MODUL2_JIDDAN/TransportasiAir.java
+++ MODUL2_JIDDAN/TransportasiAir.java
@@ -1,1 +1,24 @@
+public class TransportasiAir {
+  //This is the variable
+   protected int jumlahKursi;
+   protected int biaya;
 
+  //This is the constuction for TransportasiAir
+   public TransportasiAir(int jumlahKursi, int biaya){
+    this.jumlahKursi = jumlahKursi;
+    this.biaya = biaya;
+   }
+
+  //Method for Informasi
+   public void informasi(){
+    System.out.println();
+   }
+  //Method for Berlayar
+   public void berlayar(){
+    System.out.println();
+   }
+   //Method for Berlabuh
+   public void berlabuh(){
+    System.out.println();
+   }
+ }
